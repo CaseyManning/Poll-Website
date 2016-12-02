5376303e599e6243100ef90fa604277c38788a94
CaseyManning <kcgrind@gmail.com>
2016-12-02 06:37:28 -0800

    tracks poll answering

--- routes/create.js
+++ routes/create.js
@@ -1,32 +1,35 @@
 var express = require('express');
 var router = express.Router();
+console.log('Doing the create page thing');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log('Rendering the create page');
+
   res.render('create', {
     title: 'Create A New Poll',
     bodytext: ''
   });
+  console.log('Rendered the create page');
 });
 
 router.post('/', function(req, res, next) {
 	var db = req.db;
 	var pollcollection = db.get('polls');
-	//console.log(req);
 	console.log(req.body);
 	pollcollection.insert({
 		question: req.body.question,
 		answer1: req.body.answer1,
 		answer2: req.body.answer2,
 		answer3: req.body.answer3,
-		answer4: req.body.answer4,
+		answer4: req.body.answer4
 	});
 	res.render('create', {
 		title: 'Create A New Poll',
-		message: 'your user was created successfully!',
+		message: 'your user was created successfully!'
 		});
 	//res.send('this is a simple response: your user was created!');
-	console.log('We added the stuff')
+	console.log('We created a poll.');
 });
 
 module.exports = router;
--- routes/newuser.js
+++ routes/newuser.js
@@ -9,4 +9,22 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res, next) {
+	var db = req.db;
+	var usercollection = db.get('users');
+	//console.log(req);
+	console.log(req.body);
+	usercollection.insert({
+		username: req.body.username,
+		password: req.body.password,
+		password: req.body.password
+	});
+	res.render('answer', {
+		title: 'Answer Polls',
+		message: 'your user was created successfully!'
+		});
+	//res.send('this is a simple response: your user was created!');
+	console.log('We added the a user');
+});
+
 module.exports = router;
