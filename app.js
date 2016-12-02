ef9a8d0e84cbc04f6439e72160f4af9ab76e008d
CaseyManning <kcgrind@gmail.com>
2016-12-02 06:47:55 -0800

    fixed app.js conflict

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ var routes = require('./routes/index');
 var answer = require('./routes/answer');
 var create = require('./routes/create');
 var newuser = require('./routes/newuser');
-var login = require('./routes/login');
 
 var mongo = require('mongodb');
 var monk = require('monk');
 //var db = monk('localhost:27017/test');
 
+//var dbaddress = 'localhost:27017/test'
 // var dbaddress = process.env.MONGO_URL;
 // if (! dbaddress) {
 //   throw new Error("No MONGO_URL environment variable set!")
@@ -31,11 +31,8 @@ var db = monk(dbaddress);
 
 var app = express();
 
-var answered = [];
-
 app.use(function(req,res,next){
     req.db = db;
-    req.answered = answered;
     next();
 });
 // view engine setup
@@ -57,7 +54,6 @@ app.use('/', routes);
 app.use('/answer', answer);
 app.use('/create', create); 
 app.use('/newuser', newuser); 
-app.use('/login', login); 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
