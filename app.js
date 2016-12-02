87d1edb7d60d08579e6468fed814e2a8a1c3f01e
CaseyManning <kcgrind@gmail.com>
2016-12-01 21:05:13 -0800

    use env variable instead of hard-coded db address

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ var routes = require('./routes/index');
 var answer = require('./routes/answer');
 var create = require('./routes/create');
 var newuser = require('./routes/newuser');
+var login = require('./routes/login');
 
 var mongo = require('mongodb');
 var monk = require('monk');
 //var db = monk('localhost:27017/test');
 
- var dbaddress = 'localhost:27017/test'
 // var dbaddress = process.env.MONGO_URL;
 // if (! dbaddress) {
 //   throw new Error("No MONGO_URL environment variable set!")
@@ -22,10 +22,18 @@ var monk = require('monk');
 console.log('Using DB Address: ' + dbaddress);
 var db = monk(dbaddress);
 
+var dbaddress = process.env.MONGO_URL;
+if (! dbaddress) {
+  dbaddress = 'localhost:27017/test'
+}
+
 var app = express();
+
+var answered = [];
 
 app.use(function(req,res,next){
     req.db = db;
+    req.answered = answered;
     next();
 });
 // view engine setup
@@ -47,6 +55,7 @@ app.use('/', routes);
 app.use('/answer', answer);
 app.use('/create', create); 
 app.use('/newuser', newuser); 
+app.use('/login', login); 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
